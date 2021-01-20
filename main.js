e92eec9636a3f0e0eab798f4732e304c6f03e926
marceljones <marceljones.mj@gmail.com>
2021-01-19 19:39:57 -0600

    array examples

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Happy Tuesday People!")
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length)
+//****ASSESSING ITEMS IN ARRAY */
+console.log(students[1]);
+console.log(students[3]);
+console.log(students[students.length-1])
+
+
+students[0] = 'Mathew'
+console.log(students)
+
+//Array with a function
+
+const valuePrinter = (array, index) => {
+  return array[index];
+}
+// console.log(valuePrinter(students))
+
+const isGregHere = (arr) => {
+  return array.includes('Greg')
+};
+console.log(isGregHere(students))// 
+
